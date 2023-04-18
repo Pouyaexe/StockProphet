578a72df2a99ddc946d04cb74be1a5bbcc77e0fd
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-04-18 10:30:47 +0000

    Apr 18, 2023, 2:00 PM

--- LSTM.py
+++ LSTM.py
@@ -6,13 +6,11 @@ import pandas_ta as ta
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.preprocessing import MinMaxScaler
 
+data = yf.download(tickers = '^RUI', start = '2012-03-11',end = '2022-07-10')
+data.head(10)
 
-# Download the data
-data = yf.download(tickers='^RUI', start='2012-03-11', end='2022-07-10')
-
-# Add indicators
+# Adding indicators
 data['RSI'] = ta.rsi(data.Close, length=15)
 data['EMAF'] = ta.ema(data.Close, length=20)
 data['EMAM'] = ta.ema(data.Close, length=100)
@@ -25,64 +23,69 @@ data['TargetClass'] = [1 if data.Target[i] > 0 else 0 for i in range(len(data))]
 
 data['TargetNextClose'] = data['Adj Close'].shift(-1)
 
-# Drop unused columns
 data.dropna(inplace=True)
-data = data[['High', 'Low', 'Open', 'RSI', 'EMAF', 'EMAM', 'EMAS', 'TargetNextClose']]
+data.reset_index(inplace=True)
+data.drop(['Volume', 'Close', 'Date'], axis=1, inplace=True)
 
-# Scale the data
-sc = MinMaxScaler(feature_range=(0,1))
-data_set_scaled = sc.fit_transform(data)
+data_set = data.iloc[:, 0:11]
 
-# Create input features and targets
-X, y = [], []
+pd.set_option('display.max_columns', None)
+
+data_set.head(20)
+
+from sklearn.preprocessing import MinMaxScaler
+
+sc = MinMaxScaler(feature_range=(0, 1))
+data_set_scaled = sc.fit_transform(data_set)
+print(data_set_scaled)
+
+# multiple feature from data provided to the model
+X = []
 backcandles = 30
-for j in range(data_set_scaled.shape[1] - 1):
+print(data_set_scaled.shape[0])
+
+for j in range(8):
     X.append([])
     for i in range(backcandles, data_set_scaled.shape[0]):
-        X[j].append(data_set_scaled[i-backcandles:i, j])
-    y.append(data_set_scaled[backcandles:, -1])
-X, y = np.array(X), np.array(y).T
+        X[j].append(data_set_scaled[i - backcandles:i, j])
 
-# Split the data into train and test sets
-splitlimit = int(len(X)*0.8)
+X = np.moveaxis(X, [0], [2])
+X, yi = np.array(X), np.array(data_set_scaled[backcandles:, -1])
+y = np.reshape(yi, (len(yi), 1))
+
+splitlimit = int(len(X) * 0.8)
 X_train, X_test = X[:splitlimit], X[splitlimit:]
 y_train, y_test = y[:splitlimit], y[splitlimit:]
 
-# Convert data to PyTorch tensors
-X_train = torch.from_numpy(X_train).float()
-y_train = torch.from_numpy(y_train).float()
-X_test = torch.from_numpy(X_test).float()
-y_test = torch.from_numpy(y_test).float()
-
-# Define the model architecture
-class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super().__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
-        lstm_out, _ = self.lstm(x)
-        out = self.fc(lstm_out[:, -1, :])
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc1(out[:, -1, :])
         return out
 
-# Initialize the model, loss function, and optimizer
-model = LSTMModel(input_size=X_train.shape[2], hidden_size=150, output_size=1)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters())
+input_dim = 8
+hidden_dim = 150
+num_layers = 1
+output_dim = 1
 
-# Train the model
+model = LSTM(input_dim, hidden_dim, num_layers, output_dim)
+
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
 epochs = 30
+losses = []
+
 for epoch in range(epochs):
     optimizer.zero_grad()
-    outputs = model(X_train)
-    loss = criterion(outputs.squeeze(), y_train)
-    loss.backward()
-    optimizer.step()
-    if epoch % 10 == 0:
-        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, epochs, loss.item()))
-
-# Evaluate the model on the test set
-with torch.no_grad():
-    y_pred = model(X_test).squeeze().numpy
-
+    outputs = model(torch.Tensor(X_train))
+    loss = criterion(outputs, torch.Tensor(y_train))
